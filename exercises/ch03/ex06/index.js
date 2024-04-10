fc684594cf29c80ed9c59736b268b3341a67bb07
sen <Ushin.Sen@jp.ricoh.com>
2024-04-10 13:29:19 +0900

    ex06

--- exercises/ch03/ex06/index.js
+++ exercises/ch03/ex06/index.js
@@ -1,41 +1,44 @@
-const indexpreprocess = (str, indexStart, indexEnd) => {
-  //indexEndなし
-  if (indexEnd === undefined || indexEnd > str.length) {
-    let indexEnd = str.length
+export function substring(str, indexStart, indexEnd) {
+  let result = ""
+  //undefined
+  if (indexEnd === undefined) {
+    indexEnd = str.length
   }
   //NaN Infinity
-  if (indexStart === NaN) {
+  if (isNaN(indexStart)) {
     indexStart = 0
   }
-  if (indexEnd === NaN) {
+  if (isNaN(indexEnd)) {
     indexEnd = 0
   }
   //小数
-  let indexStartProcessed = Math.trunc(indexStart)
-  let indexEndProcessed = Math.trunc(indexEnd)
+  indexStart = Math.trunc(indexStart)
+  indexEnd = Math.trunc(indexEnd)
+  //まずは交換、そして負数の処理？
+  if (indexStart > indexEnd) {
+    [indexStart, indexEnd] = [indexEnd, indexStart]
+  }
   //負数
   if (indexStart < 0) {
     indexStart = 0
   }
   if (indexEnd < 0) {
-
+    indexEnd = str.length + indexEnd
   }
-
-}
-
-export function substring(str, indexStart, indexEnd) {
-  let result = ""
-  if (indexEnd === undefined || indexEnd > str.length) {
+  //indexEnd > str.length
+  if (indexEnd > str.length) {
     indexEnd = str.length
   }
 
+  // 結果判定
+  // indexStart>=indexEnd
   if (indexStart === indexEnd) {
     return ""
   }
-  if (indexStart > indexEnd) {
-    [indexStart, indexEnd] = [indexEnd, indexStart]
+  if (indexStart > str.length) {
+    return ""
   }
-
+  //通常処理
   for (let i = indexStart; i < indexEnd; i++) {
     result += str[i]
   }
@@ -45,15 +48,88 @@ export function substring(str, indexStart, indexEnd) {
 
 export function slice(str, indexStart, indexEnd) {
   let result = ""
-  return "TODO";
+  //前処理
+  //undefined
+  if (indexStart === undefined) {
+    indexStart = 0
+  }
+  if (indexEnd === undefined) {
+    indexEnd = str.length
+  }
+  //NaN Infinity
+  if (isNaN(indexStart)) {
+    indexStart = 0
+  }
+  if (isNaN(indexEnd)) {
+    indexEnd = 0
+  }
+  //小数
+  indexStart = Math.trunc(indexStart)
+  indexEnd = Math.trunc(indexEnd)
+  //負数
+  if (indexStart < 0) {
+    indexStart = Math.max(indexStart + str.length, 0)
+  }
+  if (indexEnd < 0) {
+    indexEnd = Math.max(str.length + indexEnd, 0)
+  }
+  //indexEnd > str.length
+  if (indexEnd > str.length) {
+    indexEnd = str.length
+  }
+
+  //結果判定
+  if (indexStart >= str.length) {
+    return ""
+  }
+  if (indexEnd <= indexStart) {
+    return ""
+  }
+
+  //通常処理
+  for (let i = indexStart; i < indexEnd; i++) {
+    result += str[i]
+  }
+  return result
 }
 
 export function padStart(str, targetLength, padString) {
-  // TODO: ここを実装しなさい
-  return "TODO";
+  let result = str
+  //padString省略される
+  if (padString === undefined) {
+    padString = " "
+  }
+
+  //
+  if (targetLength <= 0) {
+    return str
+  }
+  if (targetLength <= str.length) {
+    return str
+  }
+
+  //通常処理
+  let pad = ""
+  for (let i = 0; i < targetLength - str.length; i++) {
+    pad += padString[i % padString.length]
+  }
+  result = pad + result
+  return result
 }
 
 export function trim(str) {
-  // TODO: ここを実装しなさい
-  return "TODO";
+  let start = 0
+  let end = str.length - 1
+  let result = ""
+  while (str[start] === " ") {
+    start++
+  }
+  while (str[end] === " ") {
+    end--
+  }
+  for (let i = start; i <= end; i++) {
+    result += str[i]
+  }
+
+  return result;
 }
