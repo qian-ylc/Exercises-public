c0cc5addc3319722a701e72b3cdb10bbc0e83318
sen <Ushin.Sen@jp.ricoh.com>
2025-02-14 16:25:34 +0900

    dark mode

--- final/myClock/src/AnalogClock.tsx
+++ final/myClock/src/AnalogClock.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router';
 
 const AnalogClock: React.FC = () => {
     const [time, setTime] = useState(new Date());
+    const [darkMode, setDarkMode] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -13,6 +14,16 @@ const AnalogClock: React.FC = () => {
 
         return () => clearInterval(interval);
     }, []);
+
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add('dark');
+            document.body.classList.remove('light');
+        } else {
+            document.body.classList.add('light');
+            document.body.classList.remove('dark');
+        }
+    }, [darkMode]);
 
     const radius = 250;
     const center = radius;
@@ -36,77 +47,86 @@ const AnalogClock: React.FC = () => {
         navigate('/digi');
     };
 
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+
     // svgで時計を描画
     return (
         <>
-            <svg width={2 * (radius + borderWidth)} height={2 * (radius + borderWidth)}>
-                <circle
-                    cx={center + borderWidth}
-                    cy={center + borderWidth}
-                    r={radius}
-                    stroke="black"
-                    strokeWidth={borderWidth}
-                    fill="white"
-                />
-                {numbers.map((num) => {
-                    const angle = (360 / 12) * num;
-                    // 数字の座標
-                    const x = center + borderWidth + (radius - 30) * Math.sin((Math.PI / 180) * angle);
-                    const y = center + borderWidth - (radius - 30) * Math.cos((Math.PI / 180) * angle);
-                    // 刻みの座標
-                    const tickX1 = center + borderWidth + radius * Math.sin((Math.PI / 180) * angle);
-                    const tickY1 = center + borderWidth - radius * Math.cos((Math.PI / 180) * angle);
-                    const tickX2 = center + borderWidth + (radius - 10) * Math.sin((Math.PI / 180) * angle);
-                    const tickY2 = center + borderWidth - (radius - 10) * Math.cos((Math.PI / 180) * angle);
-                    return (
-                        <React.Fragment key={num}>
-                            <line
-                                x1={tickX1}
-                                y1={tickY1}
-                                x2={tickX2}
-                                y2={tickY2}
-                                stroke="black"
-                                strokeWidth={2}
-                            />
-                            <text
-                                x={x}
-                                y={y}
-                                textAnchor="middle"
-                                dominantBaseline="middle"
-                                fontSize="30"
-                                fontFamily="Arial"
-                            >
-                                {num}
-                            </text>
-                        </React.Fragment>
-                    );
-                })}
-                <line
-                    x1={center + borderWidth}
-                    y1={center + borderWidth}
-                    x2={center + borderWidth + hourHandLength * Math.sin((Math.PI / 180) * hourAngle)}
-                    y2={center + borderWidth - hourHandLength * Math.cos((Math.PI / 180) * hourAngle)}
-                    stroke="black"
-                    strokeWidth={6}
-                />
-                <line
-                    x1={center + borderWidth}
-                    y1={center + borderWidth}
-                    x2={center + borderWidth + minuteHandLength * Math.sin((Math.PI / 180) * minuteAngle)}
-                    y2={center + borderWidth - minuteHandLength * Math.cos((Math.PI / 180) * minuteAngle)}
-                    stroke="black"
-                    strokeWidth={4}
-                />
-                <line
-                    x1={center + borderWidth}
-                    y1={center + borderWidth}
-                    x2={center + borderWidth + secondHandLength * Math.sin((Math.PI / 180) * secondAngle)}
-                    y2={center + borderWidth - secondHandLength * Math.cos((Math.PI / 180) * secondAngle)}
-                    stroke="red"
-                    strokeWidth={2}
-                />
-            </svg>
-            <button onClick={handleChangePage}>デジタル時計</button >
+            <div>
+                <svg width={2 * (radius + borderWidth)} height={2 * (radius + borderWidth)}>
+                    <circle
+                        cx={center + borderWidth}
+                        cy={center + borderWidth}
+                        r={radius}
+                        stroke="black"
+                        strokeWidth={borderWidth}
+                        fill="none"
+                    />
+                    {numbers.map((num) => {
+                        const angle = (360 / 12) * num;
+                        // 数字の座標
+                        const x = center + borderWidth + (radius - 30) * Math.sin((Math.PI / 180) * angle);
+                        const y = center + borderWidth - (radius - 30) * Math.cos((Math.PI / 180) * angle);
+                        // 刻みの座標
+                        const tickX1 = center + borderWidth + radius * Math.sin((Math.PI / 180) * angle);
+                        const tickY1 = center + borderWidth - radius * Math.cos((Math.PI / 180) * angle);
+                        const tickX2 = center + borderWidth + (radius - 10) * Math.sin((Math.PI / 180) * angle);
+                        const tickY2 = center + borderWidth - (radius - 10) * Math.cos((Math.PI / 180) * angle);
+                        return (
+                            <React.Fragment key={num}>
+                                <line
+                                    x1={tickX1}
+                                    y1={tickY1}
+                                    x2={tickX2}
+                                    y2={tickY2}
+                                    stroke="black"
+                                    strokeWidth={2}
+                                />
+                                <text
+                                    x={x}
+                                    y={y}
+                                    textAnchor="middle"
+                                    dominantBaseline="middle"
+                                    fontSize="30"
+                                    fontFamily="Arial"
+                                >
+                                    {num}
+                                </text>
+                            </React.Fragment>
+                        );
+                    })}
+                    <line
+                        x1={center + borderWidth}
+                        y1={center + borderWidth}
+                        x2={center + borderWidth + hourHandLength * Math.sin((Math.PI / 180) * hourAngle)}
+                        y2={center + borderWidth - hourHandLength * Math.cos((Math.PI / 180) * hourAngle)}
+                        stroke="black"
+                        strokeWidth={6}
+                    />
+                    <line
+                        x1={center + borderWidth}
+                        y1={center + borderWidth}
+                        x2={center + borderWidth + minuteHandLength * Math.sin((Math.PI / 180) * minuteAngle)}
+                        y2={center + borderWidth - minuteHandLength * Math.cos((Math.PI / 180) * minuteAngle)}
+                        stroke="black"
+                        strokeWidth={4}
+                    />
+                    <line
+                        x1={center + borderWidth}
+                        y1={center + borderWidth}
+                        x2={center + borderWidth + secondHandLength * Math.sin((Math.PI / 180) * secondAngle)}
+                        y2={center + borderWidth - secondHandLength * Math.cos((Math.PI / 180) * secondAngle)}
+                        stroke="red"
+                        strokeWidth={2}
+                    />
+                </svg>
+                <div className="button-container">
+                    <button onClick={toggleDarkMode}>{darkMode ? "light mode" : "dark mode"}</button>
+                    <button className="upperRight" onClick={handleChangePage}>アナログ時計</button>
+                </div>
+            </div>
         </>
     );
 };
--- final/myClock/src/DigiClock.tsx
+++ final/myClock/src/DigiClock.tsx
@@ -5,6 +5,7 @@ import "./DigiClock.css";
 const DigiClock: React.FC = () => {
     const [time, setTime] = useState(new Date());
     const navigate = useNavigate();
+    const [darkMode, setDarkMode] = useState(false);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -30,10 +31,25 @@ const DigiClock: React.FC = () => {
 
     const display = `${hour}:${minute}:${second}`;
 
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add('dark');
+            document.body.classList.remove('light');
+        } else {
+            document.body.classList.add('light');
+            document.body.classList.remove('dark');
+        }
+    }, [darkMode]);
+
+
     return (
         <>
             <div className="button-container">
-                <button onClick={handleChangePage}>アナログ時計</button>
+                <button onClick={toggleDarkMode}>{darkMode ? "light mode" : "dark mode"}</button>
+                <button className="upperRight" onClick={handleChangePage}>アナログ時計</button>
             </div>
             <div className="digi-clock">
                 <h1>{display}</h1>
