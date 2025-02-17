dc732fdb2a0c6877d1281ecf1d4866bde5c79c46
sen <Ushin.Sen@jp.ricoh.com>
2025-02-17 20:22:37 +0900

    アラームに音をつける

--- final/myClock/src/App.tsx
+++ final/myClock/src/App.tsx
@@ -7,6 +7,8 @@ import {
   Routes
 } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import alarmSound from './assets/alarm.mp3';
+import useSound from 'use-sound';
 
 // トップレベルで管理
 // time: 現在時刻
@@ -14,8 +16,7 @@ import { useState, useEffect } from 'react';
 function App() {
   const [time, setTime] = useState(new Date());
   const [alarmTime, setAlarmTime] = useState<string | null>(null);
-  const [isAlarmRinging, setIsAlarmRinging] = useState(false);
-  const [alarmDialogOpen, setAlarmDialogOpen] = useState(false);
+  const [play, { stop }] = useSound(alarmSound);
 
   const handleSetAlarm = (timeToBeSet: string | null) => {
     if (timeToBeSet) {
@@ -24,7 +25,7 @@ function App() {
     } else {
       // clearボタンが押された場合
       localStorage.removeItem('alarmTime');
-      const timeInput = document.querySelector('input[type="time"]');
+      const timeInput: HTMLInputElement | null = document.querySelector('input[type="time"]');
       if (timeInput) {
         timeInput.value = '';
         setAlarmTime(null);
@@ -33,7 +34,11 @@ function App() {
   };
 
   const handleStopAlarm = () => {
-    setIsAlarmRinging(false);
+    const dialog: HTMLDialogElement | null = document.querySelector('#alarmRinging');
+    if (dialog) {
+      dialog.close();
+    }
+    stop();
   };
 
   useEffect(() => {
@@ -49,37 +54,57 @@ function App() {
     const savedAlarmTime = localStorage.getItem('alarmTime');
     if (savedAlarmTime) {
       setAlarmTime(savedAlarmTime);
-      const timeInput = document.querySelector('input[type="time"]') as HTMLInputElement;
+      const timeInput: HTMLInputElement | null = document.querySelector('input[type="time"]');
       if (timeInput) {
         timeInput.value = savedAlarmTime;
       }
     }
   }, []);
 
+  // 1分ごとにアラームをチェック
   useEffect(() => {
     if (alarmTime) {
       const [alarmHour, alarmMinute] = alarmTime.split(':').map(Number);
       if (time.getHours() === alarmHour && time.getMinutes() === alarmMinute) {
-        setIsAlarmRinging(true);
+        const dialog: HTMLDialogElement | null = document.querySelector('#alarmRinging');
+        if (dialog) {
+          dialog.showModal();
+        }
+        play()
       }
     }
-  }, [time, alarmTime]);
+  }, [time.getMinutes(), alarmTime]);
+
+  // 「open 属性ではなく .show() または .showModal() メソッドを使用することが推奨」
+  // ので、stateでopenの設定をしなかった
+  const openAlarmDialog = () => {
+    const dialog: HTMLDialogElement | null = document.querySelector('#alarmSetting');
+    if (dialog) {
+      dialog.showModal();
+    }
+  };
+  const closeAlarmDialog = () => {
+    const dialog: HTMLDialogElement | null = document.querySelector('#alarmSetting');
+    if (dialog) {
+      dialog.close();
+    }
+  };
 
   return (
     <BrowserRouter>
       <div className="alarm">
-        <button onClick={() => { setAlarmDialogOpen(true) }}>アラーム</button>
-        <dialog open={alarmDialogOpen}>
+        <button onClick={openAlarmDialog}>アラーム</button>
+        <dialog id="alarmSetting">
           <h2>アラーム設定</h2>
           <input
             type="time"
             onChange={(e) => handleSetAlarm(e.target.value)}
           />
           <button onClick={() => { handleSetAlarm(null) }}>clear</button>
-          <button onClick={() => { setAlarmDialogOpen(false) }}>閉じる</button>
+          <button onClick={closeAlarmDialog}>閉じる</button>
         </dialog>
       </div>
-      <dialog open={isAlarmRinging}>
+      <dialog id="alarmRinging">
         <p>Alarm ringing!</p>
         <button onClick={handleStopAlarm}>OK</button>
       </dialog>
