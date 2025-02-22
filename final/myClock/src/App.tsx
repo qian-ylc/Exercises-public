dde35daab35b96db71187c34d950043c6ed848e5
sen <Ushin.Sen@jp.ricoh.com>
2025-02-22 16:54:19 +0900

    明日の予定をチェックする機能を追加

--- final/myClock/src/App.tsx
+++ final/myClock/src/App.tsx
@@ -9,6 +9,8 @@ import {
 import { useState, useEffect } from 'react';
 import alarmSound from './assets/alarm.mp3';
 import useSound from 'use-sound';
+import token from './assets/token.json';
+import { Tooltip } from 'react-tooltip'
 
 // トップレベルで管理
 // time: 現在時刻
@@ -16,6 +18,7 @@ import useSound from 'use-sound';
 function App() {
   const [time, setTime] = useState(new Date());
   const [alarmTime, setAlarmTime] = useState<string | null>(null);
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
   const [play, { stop }] = useSound(alarmSound);
 
   const handleSetAlarm = (timeToBeSet: string | null) => {
@@ -30,6 +33,11 @@ function App() {
         timeInput.value = '';
         setAlarmTime(null);
       }
+    }
+    const timeInput: HTMLInputElement | null = document.querySelector('input[type="time"]');
+    // handleSetAlarmByCalendarの場合、時刻の入力の値を予定から取得された値に変更
+    if (timeInput && timeInput.value != timeToBeSet) {
+      timeInput.value = timeToBeSet ? timeToBeSet : '';
     }
   };
 
@@ -90,6 +98,54 @@ function App() {
     }
   };
 
+  const handleSetAlarmByCalendar = async () => {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const startDateTime = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate(), 9, 0).toISOString();
+    const endDateTime = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate(), 9, 59).toISOString();
+
+    const query = `https://graph.microsoft.com/v1.0/me/calendarView/delta?startDateTime=${startDateTime}&endDateTime=${endDateTime}`;
+    const response = await fetch(query, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token.token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP status ${response.status}`);
+    }
+
+    const data = await response.json();
+    if (data.value.length === 0) {
+      console.log('予定がありません');
+      const setAlarmByCalendarButton: HTMLButtonElement | null = document.querySelector('[data-tooltip-id="setAlarmByCalendar"]');
+      const tooltip = document.querySelector('#setAlarmByCalendar');
+      if (setAlarmByCalendarButton) {
+        // TooltipのisOpenを直接変更するのはだめ、setIsOpenで
+        if (tooltip) {
+          setIsTooltipOpen(true);
+          setTimeout(() => {
+            setIsTooltipOpen(false);
+          }, 3000);
+        }
+      }
+    } else {
+      // Recurrenceで定期イベントかどうかを判断
+      if (!data.value[0].recurrence) {
+        // 取得したのはUTCなので、日本時間に変換
+        const alarmTime = new Date(data.value[0].start.dateTime);
+        alarmTime.setHours(alarmTime.getHours() + 9);
+        const alarmTimeStr = `${String(alarmTime.getHours()).padStart(2, '0')}:${String(alarmTime.getMinutes()).padStart(2, '0')}`;
+        handleSetAlarm(alarmTimeStr);
+      } else {
+        const alarmTime = new Date(data.value[1].start.dateTime);
+        alarmTime.setHours(alarmTime.getHours() + 9);
+        const alarmTimeStr = `${String(alarmTime.getHours()).padStart(2, '0')}:${String(alarmTime.getMinutes()).padStart(2, '0')}`;
+        handleSetAlarm(alarmTimeStr);
+      }
+    }
+  }
+
   return (
     <BrowserRouter>
       <div className="alarm">
@@ -100,6 +156,8 @@ function App() {
             type="time"
             onChange={(e) => handleSetAlarm(e.target.value)}
           />
+          <button data-tooltip-id="setAlarmByCalendar" data-tooltip-content="予定がありません" onClick={handleSetAlarmByCalendar}>明日の朝</button>
+          <Tooltip id="setAlarmByCalendar" place="top" isOpen={isTooltipOpen} />
           <button onClick={() => { handleSetAlarm(null) }}>clear</button>
           <button onClick={closeAlarmDialog}>閉じる</button>
         </dialog>
@@ -112,7 +170,7 @@ function App() {
         <Route path='/' element={<AnalogClock time={time} alarmTime={alarmTime} />} />
         <Route path='/digi' element={<DigiClock time={time} alarmTime={alarmTime} />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
   )
 }
 
