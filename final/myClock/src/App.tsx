6f02121b4a4fb7f7d48808968038c91eb6d94fc1
sen <Ushin.Sen@jp.ricoh.com>
2025-02-23 18:53:43 +0900

    feat: timer

--- final/myClock/src/App.tsx
+++ final/myClock/src/App.tsx
@@ -6,7 +6,7 @@ import {
   Route,
   Routes
 } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import alarmSound from './assets/alarm.mp3';
 import useSound from 'use-sound';
 import token from './assets/token.json';
@@ -15,11 +15,19 @@ import { Tooltip } from 'react-tooltip'
 // トップレベルで管理
 // time: 現在時刻
 // alarmTime: アラーム時刻
+// isTooltipOpen: 「予定がありません」の表示
+// timer: タイマーの秒数
+// countdown: タイマーのカウントダウン秒数表示
+// timerCountdowning: タイマーがカウントダウン中かどうか
 function App() {
   const [time, setTime] = useState(new Date());
   const [alarmTime, setAlarmTime] = useState<string | null>(null);
   const [isTooltipOpen, setIsTooltipOpen] = useState(false);
   const [play, { stop }] = useSound(alarmSound);
+  const [timer, setTimer] = useState<number | null>(null);
+  const [countdown, setCountdown] = useState<string>('00:00:00');
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const [timerCountdowning, setTimerCountdowning] = useState(false);
 
   const handleSetAlarm = (timeToBeSet: string | null) => {
     if (timeToBeSet) {
@@ -49,6 +57,7 @@ function App() {
     stop();
   };
 
+  // 1秒ごとに現在時刻を更新
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date());
@@ -98,6 +107,19 @@ function App() {
     }
   };
 
+  const openTimerDialog = () => {
+    const dialog: HTMLDialogElement | null = document.querySelector('#timerSetting');
+    if (dialog) {
+      dialog.showModal();
+    }
+  };
+  const closeTimerDialog = () => {
+    const dialog: HTMLDialogElement | null = document.querySelector('#timerSetting');
+    if (dialog) {
+      dialog.close();
+    }
+  };
+
   const handleSetAlarmByCalendar = async () => {
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
@@ -117,7 +139,6 @@ function App() {
 
     const data = await response.json();
     if (data.value.length === 0) {
-      console.log('予定がありません');
       const setAlarmByCalendarButton: HTMLButtonElement | null = document.querySelector('[data-tooltip-id="setAlarmByCalendar"]');
       const tooltip = document.querySelector('#setAlarmByCalendar');
       if (setAlarmByCalendarButton) {
@@ -146,21 +167,106 @@ function App() {
     }
   }
 
+  const startTimer = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+    timerRef.current = setInterval(() => {
+      setTimer(prev => {
+        if (prev === null || prev <= 0) {
+          if (timerRef.current) {
+            clearInterval(timerRef.current);
+          }
+          return null;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (timer !== null) {
+      const hours = Math.floor(timer / 3600);
+      const minutes = Math.floor((timer % 3600) / 60);
+      const seconds = timer % 60;
+      setCountdown(`${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`);
+    } else {
+      setCountdown('00:00:00');
+    }
+  }, [timer]);
+
+  useEffect(() => {
+    if (countdown === '00:00:00' && timerCountdowning) {
+      const dialog: HTMLDialogElement | null = document.querySelector('#alarmRinging');
+      if (dialog) {
+        dialog.showModal();
+      }
+      play();
+    }
+  }, [countdown])
+
+  const handleTimerButtonClick = (seconds: number) => {
+    setTimer(seconds);
+  };
+
+  const handleTimerStart = () => {
+    if (timer !== null) {
+      startTimer();
+      setTimerCountdowning(true);
+    }
+  };
+
+  const handleTimerPause = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+  };
+
+  const handleTimerClear = () => {
+    setTimerCountdowning(false);
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+    setTimer(null);
+  };
+
   return (
     <BrowserRouter>
-      <div className="alarm">
-        <button onClick={openAlarmDialog}>アラーム</button>
-        <dialog id="alarmSetting">
-          <h2>アラーム設定</h2>
-          <input
-            type="time"
-            onChange={(e) => handleSetAlarm(e.target.value)}
-          />
-          <button data-tooltip-id="setAlarmByCalendar" data-tooltip-content="予定がありません" onClick={handleSetAlarmByCalendar}>明日の朝</button>
-          <Tooltip id="setAlarmByCalendar" place="top" isOpen={isTooltipOpen} />
-          <button onClick={() => { handleSetAlarm(null) }}>clear</button>
-          <button onClick={closeAlarmDialog}>閉じる</button>
-        </dialog>
+      <div className="Apps">
+        <div className="alarm">
+          <button onClick={openAlarmDialog}>アラーム</button>
+          <dialog id="alarmSetting">
+            <h2>アラーム設定</h2>
+            <input
+              type="time"
+              onChange={(e) => handleSetAlarm(e.target.value)}
+            />
+            <button data-tooltip-id="setAlarmByCalendar" data-tooltip-content="予定がありません" onClick={handleSetAlarmByCalendar}>明日の朝</button>
+            <Tooltip id="setAlarmByCalendar" place="top" isOpen={isTooltipOpen} />
+            <button onClick={() => { handleSetAlarm(null) }}>clear</button>
+            <button onClick={closeAlarmDialog}>閉じる</button>
+          </dialog>
+        </div>
+        <div className="timer">
+          <button onClick={openTimerDialog}>タイマー</button>
+          <dialog id="timerSetting">
+            <h2>タイマー設定</h2>
+            <div id="timers">
+              <button onClick={() => handleTimerButtonClick(30)}>30sec</button>
+              <button onClick={() => handleTimerButtonClick(60)}>1min</button>
+              <button onClick={() => handleTimerButtonClick(120)}>2min</button>
+              <button onClick={() => handleTimerButtonClick(300)}>5min</button>
+              <button onClick={() => handleTimerButtonClick(600)}>10min</button>
+            </div>
+            <div id="countdown">
+              {countdown}
+            </div>
+            <button onClick={handleTimerStart}>start</button>
+            <button onClick={handleTimerPause}>pause</button>
+            <button onClick={handleTimerClear}>clear</button>
+            <button onClick={closeTimerDialog}>閉じる</button>
+          </dialog>
+        </div>
       </div>
       <dialog id="alarmRinging">
         <p>Alarm ringing!</p>
